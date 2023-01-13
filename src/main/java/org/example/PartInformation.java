999ad832b7c2a01b22841c4a5913a89080c3d0a7
Potat-OS1 <apotat.os1@gmail.com>
2023-01-12 18:27:10 -0800

    start of messing around with enumeration

---
+++ src/main/java/org/example/PartInformation.java
@@ -0,0 +1,29 @@
+//package org.example;
+//
+//public enum PartInformation {
+//
+//    private String part;
+//
+//    PartInformation(String part) {
+//    }
+//
+//    enum RustedSet {
+//        RUSTED_ARM,
+//        RUSTED_LEG,
+//        RUSTED_BACK,
+//        RUSTED_HEAD;
+//    }
+//}
+//
+//enum Action
+//{
+//    FOO,
+//    BAR;
+//    enum MOVE
+//    {
+//        UP,
+//        DOWN,
+//        LEFT,
+//        RIGHT
+//    }
+//}
